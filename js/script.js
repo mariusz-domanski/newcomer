b30f07acdb93fd126cdf98df2d7657134a8412e5
Mariusz Domanski <mariusz@domanski.pl>
2019-06-20 21:52:36 +0200

    Create content section

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("toggleMenuButton").addEventListener("click", function () {
+    document.getElementById("mainNav").classList.toggle("toggleMenu");
+    document.getElementById("mobile-icon").classList.toggle("fa-bars");
+    document.getElementById("mobile-icon").classList.toggle("fa-times");
+});
+
+jQuery(document).ready(function($) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      var $section = $($(this).attr('href')); 
+      $('html, body').animate({
+        scrollTop: $section.offset().top
+      }, 1000);
+    }
+    $('[data-scroll]').on('click', scrollToSection);
+  }(jQuery));
